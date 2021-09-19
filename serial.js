016889c925370fd54a42bc24c9b41c6855522e51
Gautam Krishnan <gautam247gk@gmail.com>
2021-09-19 20:16:42 +0530

    1

--- serial.js
+++
@@ -1,8 +0,0 @@
-const SerialPort = require("serialport");
-const port = new SerialPort("/dev/tty-usbserial1", {
-  baudRate: 15200,
-});
-
-port.on("data", function (data) {
-  console.log("Data:", data);
-});
